1b6481c47cd571480cfa404273d00809aa1dbd96
BryanH21 <bryanhernan1019@gmail.com>
2025-02-26 02:15:59 -0500

    updated website

--- menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menu = document.querySelector(".nav-links");
-    const toggle = document.querySelector(".hamburger");
-
-    toggle.addEventListener("click", function () {
-        menu.classList.toggle("active");
-    });
-});
